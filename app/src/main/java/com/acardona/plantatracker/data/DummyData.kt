f5792b678a9d43a988bf2c5cf09424e9a394dd1e
acardo02 <00037820@uca.edu.sv>
2023-05-23 09:15:03 -0600

    feat: model layer

---
+++ app/src/main/java/com/acardona/plantatracker/data/DummyData.kt
@@ -0,0 +1,14 @@
+package com.acardona.plantatracker.data
+
+import com.acardona.plantatracker.data.model.Plant
+
+val specie = "Rosa"
+val plantColor = "Rojo"
+
+val specie1 = "Girasol"
+val plantColor1 = "Amarillo"
+
+val plants = mutableListOf(
+    Plant(specie, plantColor),
+    Plant(specie1, plantColor1)
+)
---
+++ app/src/main/java/com/acardona/plantatracker/data/model/Plant.kt
@@ -0,0 +1,6 @@
+package com.acardona.plantatracker.data.model
+
+data class Plant(
+    val specie: String,
+    val plantColor: String
+)
